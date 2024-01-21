b53569233011be44c59335609249e3dada2a00ca
Aleksandr Davydov <113263675+Davydov2577@users.noreply.github.com>
2024-01-21 21:53:15 +0200

    all

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,30 @@
-console.log("Hello, World!");
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
 
-const x = 5;
-const y = 3;
-console.log(x + y);
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
